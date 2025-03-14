3d1b76497d4923cfed4cca42d10f6fb654fa3233
Filip Stevanovic <fstevanovic7@gmail.com>
2025-03-14 14:39:26 +0100

    Config small changes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,6 +5,14 @@ import "@solarity/chai-zkit";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+  },
+  zkit: {
+    circuitsDir: "./circuits",
+  },
 };
 
 export default config;
