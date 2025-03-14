46856c7ea224e30e645ca341698b2ec90cf8e357
Filip Stevanovic <fstevanovic7@gmail.com>
2025-03-14 14:40:18 +0100

    Added APIs and util functions for deposit and withdraw - WITHDRAW STILL NOT WORKING

--- backend/server.ts
+++ backend/server.ts
@@ -1,12 +1,28 @@
 import express from "express";
 import { createTree } from "../utils/tree";
 import { ethers } from "ethers";
+import { generateCommitment } from "../utils/utils";
+import { deposit, withdraw } from "../utils/server_utils";
+import { PrivateWithdrawGroth16 } from "@zkit";
+import { pedersenHash, bigIntToBuffer, bigIntToHex } from "../utils/utils";
 import { ETHTornado, ETHTornado__factory } from "../typechain-types";
 import MerkleTree from "fixed-merkle-tree";
+
+// import { zkit } from "hardhat";
+// import hre from "hardhat";
+// import { run } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+
+async function getHRE(): Promise<HardhatRuntimeEnvironment> {
+  process.env.HARDHAT_NETWORK = "localhost"; 
+  const hre = require("hardhat");
+  return hre;  
+}
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 // Initialize the Merkle tree
 let tree: MerkleTree;
 
@@ -37,16 +53,17 @@ const depositListener = (event: any) => {
 
 }
 
-// const withdrawalListener = (event: any) => {
-//     console.log("Withdrawal Event Args:", event.args[0]);
-// }
+const withdrawalListener = (event: any) => {
+    console.log("Withdrawal Event Args:", event.args[0]);
+}
 
 tornado.on(tornado.filters.Deposit(), depositListener);
-// tornado.on(tornado.filters.Withdrawal(), withdrawalListener);
+tornado.on(tornado.filters.Withdrawal(), withdrawalListener);
 
 // Endpoint to get Merkle root and path
 app.get("/merkle-info/:commitment", (req, res) => {
   const commitment = req.params.commitment;
+  console.log("Commitment:", commitment);
   const leafIndex = tree.indexOf(commitment);
   if (leafIndex === -1) {
     res.status(404).json({ error: "Commitment not found in the tree" });
@@ -58,8 +75,89 @@ app.get("/merkle-info/:commitment", (req, res) => {
   return;
 });
 
+// Endpoint to deposit
+app.post("/deposit", async (req, res) => {
+  try {
+    const { nullifier, secret, commitment } = await generateCommitment();
+    console.log("Body:", req.body);
+    const {userAddress} = req.body;
+    console.log("User address:", userAddress);
+
+    // Initialize Hardhat runtime environment
+    const hre = await getHRE();
+
+    console.log("Using contract at address:", tornado.target);
+
+    await deposit(
+      tornado, 
+      nullifier, 
+      secret, 
+      commitment, 
+      userAddress, 
+      hre
+    );
+
+    
+    const response = {
+      nullifier: bigIntToHex(nullifier),
+      secret: bigIntToHex(secret),
+      commitment: bigIntToHex(commitment),
+    };
+
+    res.status(200).json({ message: "Deposit successful", response });
+
+  } catch (error) {
+    console.error("Error during deposit:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// Endpoint to withdraw
+app.post("/withdraw", async (req, res) => {
+  try {
+    const { nullifier, secret, root, pathIndices, pathElements, userAddress } = req.body;
+
+    // Generating recipient
+    const recipient = userAddress;
+
+    // Initialize Hardhat runtime environment
+    const hre = await getHRE();
+
+    // Create circuit representation
+    // const circuit = await hre.zkit.getCircuit("Withdraw");
+    const circuit = await hre.run("zkit:getCircuit", { name: "Withdraw" });
+    // Create inputs
+    const input: PrivateWithdrawGroth16 = {
+      root,
+      nullifierHash: await pedersenHash(bigIntToBuffer(nullifier, 31)),
+      nullifier,
+      secret,
+      pathElements/*: pathElements.map((el) => BigInt(el))*/,
+      pathIndices,
+      recipient: BigInt(recipient),
+    };
+    // Generate proof
+    const proof = await circuit.generateProof(input);
+
+    // Withdraw
+    await withdraw(
+      tornado,
+      circuit,
+      proof,
+      recipient,
+      hre
+    );
+
+    res.status(200).json({ message: "Withdraw successful" });
+  } catch (error) {
+    console.error("Error during withdraw:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // Start the server
 app.listen(PORT, async () => {
+  console.log("Provider block number:", await provider.getBlockNumber());
   await initializeTree();
   console.log(`Server running on port ${PORT}`);
 });
---
+++ utils/server_utils.ts
@@ -0,0 +1,76 @@
+import { bigIntToHex } from "../utils/utils";
+import { Tornado } from "../typechain-types";
+import { logger } from "../utils/logger";
+
+async function deposit(
+  tornado: Tornado,
+  nullifier: bigint,
+  secret: bigint,
+  commitment: bigint,
+  senderAddress: string,
+  hre: any
+) {
+  logger.startBlock("STARTING DEPOSIT PROCESS");
+
+  logger.separator();
+  logger.info(`Nullifier:  ${nullifier.toString(16)}`);
+  logger.info(`Secret:     ${secret.toString(16)}`);
+  logger.info(`Commitment: ${commitment.toString(16)}`);
+  logger.separator();
+
+  const depositAmount = hre.ethers.parseEther("1");
+  const commitmentBytes = hre.ethers.AbiCoder.defaultAbiCoder().encode(
+    ["bytes32"],
+    [bigIntToHex(commitment)]
+  );
+
+  logger.pending("\n🔄 Submitting deposit transaction...");
+
+  try {
+    // Get the signer
+    const signer = await hre.ethers.getSigner(senderAddress);
+
+    // Connect the contract instance to the signer
+    const tornadoWithSigner = tornado.connect(signer);
+
+    // Submit the deposit transaction
+    logger.info(
+      `💰 Sender balance before deposit: ${await hre.ethers.provider.getBalance(senderAddress)}`
+    );
+    await tornadoWithSigner.deposit(commitmentBytes, { value: depositAmount });
+    logger.success(
+      `🤑 Sender balance after deposit: ${await hre.ethers.provider.getBalance(senderAddress)}`
+    );
+
+    logger.endBlock("DEPOSIT SUCCESSFUL", true);
+  } catch (e: any) {
+    logger.endBlock("DEPOSIT FAILED", false);
+    logger.error(e);
+    process.exit(1);
+  }
+}
+
+import { Withdraw } from "@zkit";
+import { ProofWithdrawGroth16 } from "@zkit";
+
+async function withdraw(
+  tornado: Tornado,
+  circuit: Withdraw,
+  proof: ProofWithdrawGroth16,
+  recipient: string,
+  hre: any
+) {
+  logger.startBlock("STARTING WITHDRAW PROCESS");
+  logger.info(
+    `💰 Recipient balance before withdrawal: ${await hre.ethers.provider.getBalance(recipient)}`
+  );
+  const calldata = await circuit.generateCalldata(proof);
+  await tornado.withdraw(...calldata);
+  logger.success(
+    `🤑 Recipient balance after withdrawal: ${await hre.ethers.provider.getBalance(recipient)}`
+  );
+  logger.endBlock("WITHDRAW SUCCESSFULL");
+}
+
+
+export { deposit, withdraw };
