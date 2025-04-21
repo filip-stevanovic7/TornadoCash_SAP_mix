4e27b7d7b5cbcfecd8fb58d587c873f90ef3a9dd
Filip Stevanovic <fstevanovic7@gmail.com>
2025-04-21 15:09:17 +0200

    Updated server test script to tezt with more accoutns and deposits and withdrwas at the same time

--- scripts/server_basic_tornado_test.ts
+++ scripts/server_basic_tornado_test.ts
@@ -2,40 +2,52 @@ import axios from 'axios';
 import { ethers } from 'ethers';
 import hre from 'hardhat';
 
+const NUM_DEPOSITS = 5; // Number of deposits and withdraws to make
+
 async function main() {
   try {
     // Get the signers
-    const [deployer, user1, user2] = await hre.ethers.getSigners();
+    const users = await hre.ethers.getSigners();
+    let nullifiers = [];
+    let secrets = [];
+    let commitments = [];
   
-    // Perform a deposit
-    const depositResponse = await axios.post('http://127.0.0.1:3000/deposit', {
-      userAddress: user1.address
-    });
+    // Perform deposits
+    for(let i = 0; i < NUM_DEPOSITS; i++) {
+      const depositResponse = await axios.post('http://127.0.0.1:3000/deposit', {
+        userAddress: users[i + 1].address
+      });
 
-    console.log('Deposit Response:', depositResponse.data);
+      console.log('Deposit Response:', depositResponse.data);
 
-    const { nullifier, secret, commitment } = depositResponse.data.response;
+      const { nullifier, secret, commitment } = depositResponse.data.response;
+      nullifiers.push(nullifier);
+      secrets.push(secret);
+      commitments.push(commitment);
+    }
 
     // Small time delay to allow the server to process the deposit
     await new Promise(resolve => setTimeout(resolve, 5000));
 
     // Get Merkle root and path
-    const merkleInfoResponse = await axios.get(`http://127.0.0.1:3000/merkle-info/${commitment}`);
-    console.log('Merkle Info Response:', merkleInfoResponse.data);
+    for(let i = 0; i < NUM_DEPOSITS; i++) {
+      const merkleInfoResponse = await axios.get(`http://127.0.0.1:3000/merkle-info/${commitments[i]}`);
+      console.log('Merkle Info Response:', merkleInfoResponse.data);
 
-    const { root, pathElements, pathIndices } = merkleInfoResponse.data;
+      const { root, pathElements, pathIndices } = merkleInfoResponse.data;
 
-    // Perform a withdrawal
-    const withdrawResponse = await axios.post('http://127.0.0.1:3000/withdraw', {
-      nullifier,
-      secret,
-      root,
-      pathElements,
-      pathIndices,
-      userAddress: user2.address
-    });
+      // Perform a withdrawal
+      const withdrawResponse = await axios.post('http://127.0.0.1:3000/withdraw', {
+        nullifier: nullifiers[i],
+        secret: secrets[i],
+        root,
+        pathElements,
+        pathIndices,
+        userAddress: users[i + 1 + NUM_DEPOSITS].address
+      });
 
-    console.log('Withdraw Response:', withdrawResponse.data);
+      console.log('Withdraw Response:', withdrawResponse.data);
+    }
 
   } catch (error) {
     console.error('Error in test script:', error);
